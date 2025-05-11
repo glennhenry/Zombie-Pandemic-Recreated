4d0ec523f52809e5aaae18dc669f3947d2c99b92
glennhenry <glennhenrybdg05@gmail.com>
2025-05-11 22:26:23 +0700

    Add back button mainpage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
+import LightHyperlink from "../components/ui/LightHyperlink";
+
 export default function NotFound() {
   return (
-    <div className="flex h-[100dvh] items-center justify-center">
-      <h1 className="text-xl">404 Not Found</h1>
+    <div className="flex h-[100dvh] flex-col items-center justify-center">
+      <h1 className="text-3xl text-black">404 Not Found</h1>
+      <LightHyperlink href="/" target="_self" children={"Back"} />
     </div>
   );
 }
