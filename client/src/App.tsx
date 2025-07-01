cd4f15a1071ca98a09233d88245ef3a6182ac084
glennhenry <glennhenrybdg05@gmail.com>
2025-07-01 22:58:45 +0700

    Add background image and footer, add example .tsx file to show project structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,45 @@
-import { useEffect, useState } from "react";
-import { ASSETS } from "./utils/assets";
-import { BASE_URL } from "./utils/config";
-
+import Example1 from "./ui/components/ex1";
+import Example2 from "./ui/features/ex2";
 
 export default function App() {
-  const [apiResponse, setApiResponse] = useState(null)
-
-  useEffect(() => {
-    fetch(`${BASE_URL}/testapi`)
-      .then(res => res.json())
-      .then(res => setApiResponse(res))
-      .catch(err => console.error('/testapi fails:', err))
-  }, [])
-
   return (
-    <div className="bg-b-base">
-      <div className="bg-b-base">
-        <header className="container">
+    <div className="bg-[url(/src/resources/bg.jpg)] bg-cover">
+      <div className="">
+        <header className="container ">
           <h1>Header</h1>
         </header>
       </div>
 
-      <main className="min-h-[100vh] container bg-darker">
+      <main className="min-h-[100vh] container">
         <h1>MAIN CONTENT</h1>
-        <img src={ASSETS.profile.avatar} />
-        {apiResponse
-          ? (<h1>/testapi: {JSON.stringify(apiResponse)}</h1>)
-          : (<h1>Loading API response...</h1>)
-        }
+        <Example1 />
+        <Example2 />
       </main>
 
-      <div className="bg-b-base">
-        <footer className="container">
-          <h1>Footer copyright</h1>
+      <div className="bg-[#131313] text-gray-400 py-4">
+        <footer className="container flex flex-col items-center justify-between gap-2 text-sm md:flex-row md:gap-0 px-4">
+          <p className="text-center">
+            <strong>ZP Recreated</strong> — Not affiliated with original developers
+          </p>
+
+          <div className="flex gap-4 underline">
+            <a
+              href="https://github.com/glennhenry/Zombie-Pandemic-Recreated"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white transition"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://discord.com/invite/Yrzsk7n6nf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white transition"
+            >
+              Discord
+            </a>
+          </div>
         </footer>
       </div>
     </div>
