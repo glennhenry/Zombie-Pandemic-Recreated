1a527e718fd1429e75905d98d44b3db05e5075c6
glennhenry <glennhenrybdg05@gmail.com>
2025-07-04 00:42:14 +0700

    fix webpage not served and conflicting assets directory in prod by renaming

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "../build/web",
+    assetsDir: "web-assets",
     emptyOutDir: true,
   },
 });
--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -6,6 +6,7 @@ import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
+import java.io.File
 
 object Data {
     val x = 1
@@ -16,14 +17,15 @@ data class ZPResponse(val a: Int)
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-
         get("/testapi") {
             call.respond(HttpStatusCode.OK, ZPResponse(Data.x))
         }
 
-        staticResources("/assets", "assets")
+        staticResources("/assets", "game-assets")
+
+        // Serve the static website only in production; the development environment will use the Vite server.
+        if (!application.developmentMode) {
+            staticFiles("/", File("web"), index = "index.html")
+        }
     }
 }
