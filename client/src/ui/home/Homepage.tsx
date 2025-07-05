765ed26b23cf95637ea02d3cbfd5a69f01183487
glennhenry <glennhenrybdg05@gmail.com>
2025-07-05 20:50:24 +0700

    Add play button in home page

--- client/src/ui/home/Homepage.tsx
+++ client/src/ui/home/Homepage.tsx
@@ -1,10 +1,13 @@
-import textLinearGradientStyle from "../../utils/textGradient";
+import {
+  textLinearGradientStyle,
+  bglinearGradientStyle,
+} from "../../utils/linearGradient";
 import textStrokeStyle from "../../utils/textStroke";
 
 export default function Homepage() {
   return (
-    <>
-      <div className="p-3 text-center">
+    <div className="flex flex-col">
+      <div className="noselect p-3 text-center">
         <h1
           className="font-game-logo text-7xl"
           style={textLinearGradientStyle(
@@ -24,6 +27,32 @@ export default function Homepage() {
           RECREATED
         </h1>
       </div>
-    </>
+      <div
+        className="flex h-17 items-center justify-center rounded-md"
+        style={bglinearGradientStyle(
+          "bottom",
+          "--color-play-bg-gradient-start",
+          "--color-play-bg-gradient-end",
+          0,
+          100,
+        )}
+      >
+        <a
+          href="/play"
+          className="playbtn-shadow inline-block cursor-pointer rounded-md px-10 py-2"
+          style={bglinearGradientStyle(
+            "bottom",
+            "--color-play-btn-gradient-start",
+            "--color-play-btn-gradient-end",
+            0,
+            100,
+          )}
+        >
+          <p className="font-russo text-lg" style={textStrokeStyle(2, "black")}>
+            PLAY
+          </p>
+        </a>
+      </div>
+    </div>
   );
 }
