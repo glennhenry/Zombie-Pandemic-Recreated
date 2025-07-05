76828d87ce00393eb0fa2b3897f8e6889ce5de88
glennhenry <glennhenrybdg05@gmail.com>
2025-07-05 21:36:24 +0700

    Set grid layout for homepage cards

--- client/src/ui/home/Homepage.tsx
+++ client/src/ui/home/Homepage.tsx
@@ -3,10 +3,11 @@ import {
   bglinearGradientStyle,
 } from "../../utils/linearGradient";
 import textStrokeStyle from "../../utils/textStroke";
+import { HomeCard } from "../components/Card";
 
 export default function Homepage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="noselect p-3 text-center">
         <h1
           className="font-game-logo text-7xl"
@@ -52,6 +53,49 @@ export default function Homepage() {
             PLAY
           </p>
         </a>
+      </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+          <div className="flex flex-col gap-2">
+            <HomeCard className="">
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+            </HomeCard>
+            <HomeCard className="mb-4 break-inside-avoid">
+              <p>News</p>
+            </HomeCard>
+          </div>
+          <div className="flex flex-col gap-2">
+            <HomeCard className="mb-4 break-inside-avoid">
+              <p>Login/Register</p>
+            </HomeCard>
+            <HomeCard className="mb-4 break-inside-avoid">
+              <p>About</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+              <p>Preview</p>
+            </HomeCard>
+          </div>
+        </div>
       </div>
     </div>
   );
