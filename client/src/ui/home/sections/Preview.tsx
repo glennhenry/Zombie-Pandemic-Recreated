02ce99673eaccc9dc8e96a7b5e2c9bbcef227cc6
glennhenry <glennhenrybdg05@gmail.com>
2025-07-10 00:22:59 +0700

    Create gameplay preview for homepage

--- client/src/ui/home/sections/Preview.tsx
+++ client/src/ui/home/sections/Preview.tsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import { Carousel } from "../../components/Carousel";
+import type { Image } from "../../../utils/type";
+
+const previewImagePaths = [
+  "/gameplays/gameplay-1.jpg",
+  "/gameplays/gameplay-2.jpg",
+  "/gameplays/gameplay-3.jpg",
+  "/gameplays/gameplay-4.jpg",
+  "/gameplays/gameplay-1.jpg",
+  "/gameplays/gameplay-2.jpg",
+];
+
+const images = previewImagePaths.map((_path) => {
+  const image: Image = { path: _path, alt: "ZP Recreated gameplay" };
+  return image;
+});
+
 export default function Preview() {
-  return <h1>Preview</h1>;
+  const [previewIndex, setPreviewIndex] = useState(0);
+
+  return (
+    <div className="flex w-120 max-w-full flex-col gap-2">
+      <div>
+        <img
+          src={previewImagePaths[previewIndex]}
+          alt="ZP Recreated gameplay"
+          className="object-contain"
+        />
+      </div>
+      <Carousel
+        images={images}
+        selected={previewIndex}
+        onSelect={(idx) => setPreviewIndex(idx)}
+      />
+    </div>
+  );
 }
