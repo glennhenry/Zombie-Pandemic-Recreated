b7f23dd429689b75af80e1155b341ac8b1dd2f99
glennhenry <glennhenrybdg05@gmail.com>
2025-07-10 21:13:53 +0700

    Create simple authentication fields

--- client/src/ui/home/sections/Auth.tsx
+++ client/src/ui/home/sections/Auth.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { Button } from "../../components/Button";
+import { TextField } from "../../components/TextField";
+import { FiEye } from "react-icons/fi";
+
 export default function Auth() {
-  return <h1>Auth</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [hidePassword, setHidePassword] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <h1>Login/Register</h1>
+      <TextField
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextField
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        placeholder="Password"
+        hide={hidePassword}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        onToggleHide={() => setHidePassword(!hidePassword)}
+      >
+        <FiEye size={18} />
+      </TextField>
+    </div>
+  );
 }
