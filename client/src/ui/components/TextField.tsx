74eea18b2f437261201f4bdd17612a71abc21dbc
glennhenry <glennhenrybdg05@gmail.com>
2025-07-10 21:13:38 +0700

    Implement text field component

---
+++ client/src/ui/components/TextField.tsx
@@ -0,0 +1,33 @@
+import classNames from "../../utils/ui/classNames";
+
+interface TextFieldProps {
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  hide?: boolean;
+  className?: string;
+  onToggleHide?: () => void;
+  children?: React.ReactNode;
+}
+
+export const TextField = (props: TextFieldProps) => {
+  return (
+    <div className={`relative ${classNames(props.className)}`}>
+      <input
+        value={props.value}
+        onChange={props.onChange}
+        type={props.hide ? "password" : "text"}
+        placeholder={props.placeholder}
+        className="w-full rounded-sm bg-gray-100 p-2 pr-10 text-gray-700 placeholder-gray-400"
+      />
+      {props.children && (
+        <div
+          onClick={props.onToggleHide}
+          className="absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer text-gray-500"
+        >
+          {props.children}
+        </div>
+      )}
+    </div>
+  );
+};
