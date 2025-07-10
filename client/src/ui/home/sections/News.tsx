08b5c9798b3c98964cc0845afce46ec7448ba1e6
glennhenry <glennhenrybdg05@gmail.com>
2025-07-10 20:38:48 +0700

    Fetch text from release-notes.md instead, adjust spacing, add isTrimmed state to show "and more" text

--- client/src/ui/home/sections/News.tsx
+++ client/src/ui/home/sections/News.tsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 
 const RELEASE_NOTES_URL =
-  "https://raw.githubusercontent.com/glennhenry/Zombie-Pandemic-Recreated/main/README.md";
+  "https://raw.githubusercontent.com/glennhenry/Zombie-Pandemic-Recreated/main/release-notes.md";
 
 export default function News() {
   const [lines, setLines] = useState<string[]>([]);
+  const [isTrimmed, setIsTrimmed] = useState<boolean>(false);
 
   useEffect(() => {
     fetch(RELEASE_NOTES_URL)
       .then((res) => res.text())
       .then((text) => {
         const allLines = text.trim().split("\n");
-        setLines(allLines.slice(0, 30));
+        setIsTrimmed(allLines.length > 50);
+        setLines(allLines.slice(0, 50));
       })
       .catch(console.error);
   }, []);
 
   const renderLine = (line: string, idx: number) => {
     if (line.startsWith("#")) {
+      const headingTopMargin = idx !== 0 ? "mt-3" : "mt-0";
+
       return (
-        <h2 key={idx} className="text-md mt-3 font-bold">
+        <h2 key={idx} className={`text-md ${headingTopMargin} font-bold`}>
           {line.replace(/^#+\s*/, "")}
         </h2>
       );
@@ -29,11 +33,9 @@ export default function News() {
           {line.slice(1).trim()}
         </li>
       );
-    } else if (line.trim() === "") {
-      return <br key={idx} />;
     } else {
       return (
-        <p key={idx} className="text-sm">
+        <p key={idx} className="mt-2 text-sm">
           {line}
         </p>
       );
@@ -44,16 +46,17 @@ export default function News() {
     <div className="flex flex-col gap-2">
       <h1 className="font-paragraph text-xl">News</h1>
 
-      <div className="max-h-96 overflow-y-auto p-3 text-sm whitespace-pre-wrap text-paragraph">
+      <div className="max-h-96 overflow-y-auto p-1 text-sm whitespace-pre-wrap text-paragraph">
         {lines.map((line, idx) => renderLine(line, idx))}
 
         <p>
-          ...and more{"\n\n"}
+          {isTrimmed && <p>...and more{"\n\n"}</p>}
+
           <a
             href="https://github.com/glennhenry/Zombie-Pandemic-Recreated/blob/main/release-notes.md"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-paragraph underline"
+            className="mt-4 text-paragraph underline"
           >
             See full →
           </a>
