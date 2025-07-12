d8a8c733a6c3adb99b5601894dbd0526131de776
glennhenry <glennhenrybdg05@gmail.com>
2025-07-12 20:21:56 +0700

    Tidy up auth fields, create keep logged in and forgot password line

--- client/src/ui/home/sections/Auth.tsx
+++ client/src/ui/home/sections/Auth.tsx
@@ -2,35 +2,62 @@ import { useState } from "react";
 import { Button } from "../../components/Button";
 import { TextField } from "../../components/TextField";
 import { FiEye } from "react-icons/fi";
+import replaceClick from "../../../utils/replaceClick";
+import { Checkbox } from "../../components/Checkbox";
 
 export default function Auth() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [hidePassword, setHidePassword] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [keepLogin, setKeepLogin] = useState(false);
 
   return (
-    <div className="flex flex-col gap-3">
-      <h1>Login/Register</h1>
-      <TextField
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        placeholder="Password"
-        hide={hidePassword}
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        onToggleHide={() => setHidePassword(!hidePassword)}
-      >
-        <FiEye size={18} />
-      </TextField>
+    <div className="flex flex-col gap-4">
+      <h1 className="text-xl">Login/Register</h1>
+      <div className="flex flex-col items-center gap-3">
+        {isRegistering && (
+          <TextField
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full max-w-90"
+          />
+        )}
+        <TextField
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="w-full max-w-90"
+        />
+
+        <TextField
+          placeholder="Password"
+          hide={hidePassword}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onToggleHide={() => setHidePassword(!hidePassword)}
+          className="w-full max-w-90"
+        >
+          <FiEye size={18} />
+        </TextField>
+        <div className="flex w-full max-w-90 justify-between">
+          <Checkbox
+            isChecked={keepLogin}
+            onChecked={(e) => setKeepLogin(e.target.checked)}
+          >
+            <p>Keep logged in</p>
+          </Checkbox>
+          <a
+            href="#"
+            className="emphasized link"
+            onClick={(e) => replaceClick(e)}
+          >
+            Forgot password
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
