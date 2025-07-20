3730e8007dc9833fd85659157affcba52ebbbdc4
glennhenry <glennhenrybdg05@gmail.com>
2025-07-20 22:08:37 +0700

    Set base layout of the game container

--- client/src/ui/features/Game.tsx
+++ client/src/ui/features/Game.tsx
@@ -1,14 +1,31 @@
 import type { Account } from "../../core/types/Account";
+import { bglinearGradientStyle } from "../../utils/ui/linearGradient";
 
 interface GameProps {
-    account?: Account | null;
+  account?: Account | null;
 }
 
 export default function Game(_props: GameProps) {
   return (
     // Game container
-    <div className="mx-auto h-[800px] w-full max-w-[1200px] min-w-[800px] bg-amber-800">
-      
+    <div className="mx-auto h-[800px] w-full max-w-[1200px] min-w-[800px] border-1 border-gray-500 bg-[#d4d4d4]">
+      {/* Topbar */}
+      <div
+        className="h-20"
+        style={bglinearGradientStyle(
+          "bottom",
+          "--color-topbar-gradient-start",
+          "--color-topbar-gradient-end",
+          36,
+          80,
+        )}
+      >
+        <p>Top bar</p>
+      </div>
+      {/* Game view*/}
+      <div className="">
+        <p>Game view</p>
+      </div>
     </div>
   );
 }
