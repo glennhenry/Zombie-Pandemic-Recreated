9f0fe214d58c98bed9c06a54444c7baf4167b0e2
glennhenry <glennhenrybdg05@gmail.com>
2025-07-20 22:08:14 +0700

    Add topbar collapse

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,16 @@ import Game from "./ui/features/Game";
 import Homepage from "./ui/home/Homepage";
 import replaceClick from "./utils/replaceClick";
 import { Account } from "./core/types/Account";
-import { clearAccountCookie, getAccountFromCookie, saveAccountToCookie } from "./utils/accountSession";
+import {
+  clearAccountCookie,
+  getAccountFromCookie,
+  saveAccountToCookie,
+} from "./utils/accountSession";
+import { FaAngleDoubleDown, FaAngleDoubleUp } from "react-icons/fa";
 
 export default function App() {
   const [account, _setAccount] = useState<Account | null>(null);
+  const [topbarClosed, setTopbarClosed] = useState<boolean>(false);
 
   const renderPage = () => {
     switch (window.location.pathname) {
@@ -40,41 +46,52 @@ export default function App() {
 
   return (
     <div className="bg-[#050505] bg-[url(/bg.jpg)] bg-contain bg-top bg-no-repeat">
-      <div className="flex items-center justify-between bg-app-bar px-4 text-right text-xs">
-        <a className="font-game-logo text-2xl" href="/" title="Home">
-          ZP
-        </a>
-        {account ? (
-          <p>
-            Logged in as: <span className="emphasized">{account.username}</span>
-            {" - "}
-            <span className="emphasized">{account.email}</span>
-            {" | "}
-            <button
-              onClick={(_) => {
-                if (window.location.pathname === "/play") {
-                  const confirmLogout = window.confirm(
-                    "This will disconnect you from the game. Continue?",
-                  );
-                  if (!confirmLogout) return;
-                }
+      {!topbarClosed && (
+        <div className="flex items-center justify-between bg-app-bar px-4 text-right text-xs">
+          <a className="font-game-logo text-2xl" href="/" title="Home">
+            ZP
+          </a>
+          {account ? (
+            <p>
+              Logged in as:{" "}
+              <span className="emphasized">{account.username}</span>
+              {" - "}
+              <span className="emphasized">{account.email}</span>
+              {" | "}
+              <button
+                onClick={(_) => {
+                  if (window.location.pathname === "/play") {
+                    const confirmLogout = window.confirm(
+                      "This will disconnect you from the game. Continue?",
+                    );
+                    if (!confirmLogout) return;
+                  }
 
-                setAccount(null);
-                replaceClick(undefined, "Remove account state from server");
+                  setAccount(null);
+                  replaceClick(undefined, "Remove account state from server");
 
-                if (window.location.pathname === "/play") {
-                  window.location.pathname = "/";
-                }
-              }}
-              className="emphasized link"
-            >
-              logout
-            </button>
-          </p>
-        ) : (
-          <p>Not logged in</p>
-        )}
-      </div>
+                  if (window.location.pathname === "/play") {
+                    window.location.pathname = "/";
+                  }
+                }}
+                className="emphasized link"
+              >
+                logout
+              </button>
+            </p>
+          ) : (
+            <p>Not logged in</p>
+          )}
+        </div>
+      )}
+
+      <button
+        onClick={() => setTopbarClosed(!topbarClosed)}
+        className={`absolute ${topbarClosed ? "top-2" : "top-10"} left-2 z-10 flex items-center justify-center rounded-md bg-white/80 p-1 text-gray-700 shadow-md hover:bg-white active:scale-95`}
+        title={topbarClosed ? "Show topbar" : "Hide topbar"}
+      >
+        {topbarClosed ? <FaAngleDoubleDown /> : <FaAngleDoubleUp />}
+      </button>
 
       <main className="container min-h-[100vh] pb-20">{renderPage()}</main>
 
