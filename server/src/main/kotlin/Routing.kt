356fa590d5cc530765ca0c191840fe86c5b7235d
glennhenry <glennhenrybdg05@gmail.com>
2025-09-09 15:59:45 +0700

    Create app base routes

--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -1,26 +1,51 @@
 package dev.zprecreated
 
-import dev.zprecreated.api.game.gameMetadataRoutes
-import dev.zprecreated.api.game.map.mapRoutes
+import dev.zprecreated.model.account.PlayerAccount
+import dev.zprecreated.ui.CommonHead
+import dev.zprecreated.ui.home.HomePage
+import dev.zprecreated.ui.layout.BaseLayout
+import dev.zprecreated.ui.play.PlayPage
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import kotlinx.html.body
+import kotlinx.html.head
 import java.io.File
 
 fun Application.configureRouting() {
-    routing {
-        if (application.developmentMode) {
-            staticFiles("assets", File("server/game-assets"))
-        } else {
-            staticFiles("assets", File("game-assets"))
-        }
-        gameMetadataRoutes()
-        mapRoutes()
+    val account = PlayerAccount.guest()
 
-        // Serve the static website only in production; the development environment will use the Vite server.
-        if (!application.developmentMode) {
-            singlePageApplication {
-                react("web")
+    routing {
+        // assets directory is on static/
+        // for compactness, routes use assets/ (for web assets) and game/ (for game assets)
+        // which will be redirected to the corresponding directory
+        staticFiles("assets", File("server/static/web-assets"))
+        staticFiles("game", File("server/static/game-assets"))
+
+        get("/") {
+            call.respondHtml {
+                head {
+                    CommonHead(play = false)
+                }
+                body {
+                    BaseLayout(account, topbarClosed = false) {
+                        HomePage()
+                    }
+                }
+            }
+        }
+
+        get("/play") {
+            call.respondHtml {
+                head {
+                    CommonHead(play = true)
+                }
+                body {
+                    BaseLayout(account, topbarClosed = false) {
+                        PlayPage()
+                    }
+                }
             }
         }
     }
