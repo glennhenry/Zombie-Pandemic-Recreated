262357f7f591e0ea89739576cbe9c82477d59bd3
glennhenry <glennhenrybdg05@gmail.com>
2025-09-10 14:15:28 +0700

    Create HomeCard component

--- server/src/main/kotlin/ui/home/HomeCard.kt
+++ server/src/main/kotlin/ui/home/HomeCard.kt
@@ -6,10 +6,9 @@ import kotlinx.html.div
 
 data class HomeCardProps(
     val classes: String = "",
-    val content: FlowContent.() -> Unit
 )
 
-fun FlowContent.HomeCard(props: HomeCardProps, content: FlowContent.() -> Unit) {
+fun FlowContent.HomeCard(props: HomeCardProps = HomeCardProps(), content: FlowContent.() -> Unit) {
     div(classes = "${Styles.bg(Styles.colorBaseContainer)} p-4 border-2 border-[#A1A1A1] ${props.classes}") {
         content()
     }
