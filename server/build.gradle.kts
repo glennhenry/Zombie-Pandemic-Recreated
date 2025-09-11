aad93c87f69e4756d0493b1f679be594386b058f
glennhenry <glennhenrybdg05@gmail.com>
2025-09-11 13:27:43 +0700

    Add Ktor client CIO engine dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
     implementation(libs.ktor.server.html.dsl)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
--- server/src/main/kotlin/GlobalContext.kt
+++ server/src/main/kotlin/GlobalContext.kt
@@ -1,7 +1,8 @@
 package dev.zprecreated
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
 
 object GlobalContext {
-    val httpClient = HttpClient()
+    val httpClient = HttpClient(CIO)
 }
